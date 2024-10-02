76492e02ff6ee556a4255427cab855c824483634
Liam Hinzman <liamhinzman@gmail.com>
2024-10-02 16:02:34 -0400

    Starview bay update

--- map.js
+++ map.js
@@ -92,7 +92,7 @@ const infos = {
     'image': '/images/img-mcba.jpg',
     'logo': '/images/logo-mcba.svg',
     'title': 'Landmark',
-    'lead': '172 units (office)',
+    'lead': '50 Units (1-2br)',
     'description': `Ducros, Gleyre, Steinlen, Vallotton and Soutter: these are some of the Vaudois painters for which the Vaud Museum of Fine Arts (MCBA) is known, nationally and internationally. Their works form a large part of the museum’s collection, currently comprising some 10,000 paintings. <br><br>
     The MCBA organises several temporary exhibitions every year, each one accompanied with a programme of cultural activities including events, guided tours and workshops for adults and children. Admission is free on the first Saturday of the month.`,
     'contact': [
@@ -118,7 +118,21 @@ const infos = {
     'image': '/images/img-mudac.jpg',
     'logo': '/images/logo-mudac.svg',
     'title': 'Ataraxia Park 13-G',
-    'lead': '257 Units (1br)',
+    'lead': '300 Units (2br)',
+    'description': `It has developed its identity and international reputation through hundreds of ambitious and often unusual exhibitions and continues to pursue a policy of openness and exchange between the many disciplines of contemporary creation. <br><br>
+    Its programme showcases designers and artists at solo exhibitions and extends to exhibitions that question the public on contemporary social issues. It demonstrates the museum’s interest in the world and in the wide scope that the term design itself can encompass. <br><br>
+    The diversity of points of view of each project has enabled mudac to assert itself both on the national and international scene by offering its exhibitions to its counterparts. Europe and Asia have thus been able to discover exhibitions made in mudac on numerous occasions.`,
+    'contact': [
+        '+41 21 318 44 00',
+        'mudac@plateforme10.ch'
+    ],
+    'website': 'https://mudac.ch/en/'
+    },
+    "starviewBay": {
+    'image': '/images/img-mudac.jpg',
+    'logo': '/images/logo-mudac.svg',
+    'title': 'Starview Bay A1 & A4',
+    'lead': '800 units (1br)',
     'description': `It has developed its identity and international reputation through hundreds of ambitious and often unusual exhibitions and continues to pursue a policy of openness and exchange between the many disciplines of contemporary creation. <br><br>
     Its programme showcases designers and artists at solo exhibitions and extends to exhibitions that question the public on contemporary social issues. It demonstrates the museum’s interest in the world and in the wide scope that the term design itself can encompass. <br><br>
     The diversity of points of view of each project has enabled mudac to assert itself both on the national and international scene by offering its exhibitions to its counterparts. Europe and Asia have thus been able to discover exhibitions made in mudac on numerous occasions.`,
